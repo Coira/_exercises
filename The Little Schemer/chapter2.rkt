63edbce80aa98680e9c357cf77fa7df7e9d4f2f7
Coira <coira.ells@gmail.com>
2015-12-20 14:40:17 +0000

    initial commit

---
+++ The Little Schemer/chapter2.rkt
@@ -0,0 +1,23 @@
+#lang racket
+
+(define atom?
+  (lambda (x)
+    (and (not (pair? x)) (not (null? x)))))
+
+(define lat?
+  (lambda (l)
+    (cond
+      ((null? l) #t)
+      ((atom? (car l)) (lat? (cdr l)))
+      (else #f))))
+  
+
+(lat? '(bacon and eggs))
+
+(define member?
+  (lambda (a lat)
+    (cond
+      ((null? lat) #f)
+      (else (or (eq? (car lat) a)
+                (member? a (cdr lat)))))))
+
